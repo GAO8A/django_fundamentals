0e4411777d64cc8dbe815ac85bfb01a4b7af60c0
Rick <rikgao@icloud.com>
2018-09-04 23:34:21 -0400

    added user invite

--- tictactoe/player/urls.py
+++ tictactoe/player/urls.py
@@ -1,6 +1,6 @@
 from django.urls import re_path
 from django.contrib.auth.views import LoginView, LogoutView
-from .views import home, new_invitation
+from .views import home, new_invitation, accept_invitation
 
 urlpatterns = [
     re_path(r'home$', home, name="player_home"),
@@ -10,5 +10,9 @@ urlpatterns = [
     re_path(r'logout$',
             LogoutView.as_view(),  # convert to function, in a class based view
             name="player_logout"),  # name
-    re_path(r'new_invitation$', new_invitation, name="player_new_invitation")
+    re_path(r'new_invitation$', new_invitation, name="player_new_invitation"),
+    # matches one or more digits eg. accept_invitations/12/
+    re_path(r'accept_invitations/(?P<id>\d+)/$',
+            accept_invitation,
+            name="player_accept_invitation")
 ]
